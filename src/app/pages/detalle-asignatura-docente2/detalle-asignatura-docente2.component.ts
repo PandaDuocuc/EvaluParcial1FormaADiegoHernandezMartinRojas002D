6dd8f9d0f9b01dc40bb1c0967628fa52796853d1
Diego Hernández <di.hernandezl@duocuc.cl>
2024-09-27 19:56:51 -0300

    Todo menos restablecer contraseña con mejora textual

--- src/app/pages/detalle-asignatura-docente2/detalle-asignatura-docente2.component.ts
+++ src/app/pages/detalle-asignatura-docente2/detalle-asignatura-docente2.component.ts
@@ -17,8 +17,8 @@ export class DetalleAsignaturaDocente2Component {
     { dia: 'Miércoles', horaInicio: '13:01', horaFin: '15:50' }
   ];
   contenidos: string[] = [
-    'Experiencia de Aprendizaje 1 - ',
-    'Experiencia de Aprendizaje 2 - Garantizando la calidad del software: Aplicación de conceptos y diseño de casos de prueba',
+    'Experiencia de Aprendizaje 1 - Aseguramiento de calidad del software: Conceptos, pruebas y planificación',
+    'Experiencia de Aprendizaje 2 - Garantizando la calidad del software: Aplicación de conceptos y diseño de casos de prueba efectivos',
     'Experiencia de Aprendizaje 3 - Optimizando la ejecución de pruebas: Técnicas, seguridad y documentación para garantizar la calidad del software'
   ];
 
